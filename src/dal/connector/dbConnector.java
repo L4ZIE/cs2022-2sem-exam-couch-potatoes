2cbde73ab8babce28e863afce983de2021102dd9
Bruger <davidwymon@yahoo.com>
2023-04-27 10:04:33 +0200

    dbConnecter added

---
+++ src/dal/connector/dbConnector.java
@@ -0,0 +1,53 @@
+package dal.connector;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class dbConnector {
+    private static final String file = "resources/sqllogin.properties";
+
+    private SQLServerDataSource dataSource = null;
+
+    private static dbConnector instance;
+
+    public static dbConnector getInstance(){
+        if(instance==null){
+            instance = new dbConnector();
+        }
+        return instance;
+    }
+
+    private dbConnector(){
+        Properties properties = getConnectionDetails();
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName(properties.getProperty("name"));
+        dataSource.setUser(properties.getProperty("username"));
+        dataSource.setPassword(properties.getProperty("password"));
+        dataSource.setServerName(properties.getProperty("server"));
+        dataSource.setPortNumber(Integer.parseInt(properties.getProperty("port")));
+        dataSource.setTrustServerCertificate(true);
+    }
+    public Connection createConnection()throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+    private Properties getConnectionDetails() {
+        Properties properties = new Properties();
+
+        try {
+            FileInputStream sr = new FileInputStream(file);
+            properties.load(sr);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+}
