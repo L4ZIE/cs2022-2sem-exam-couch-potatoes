bf18a1cb3a579370b52fbc755561ccaabf4e47ce
Bálint <47783658+Roharx@users.noreply.github.com>
2023-04-30 02:27:28 +0200

    Created PictureDAO base functions

--- src/dal/interfaces/IPictureDAO.java
+++ src/dal/interfaces/IPictureDAO.java
@@ -1,4 +1,10 @@
 package dal.interfaces;
 
+import java.util.ArrayList;
+
 public interface IPictureDAO {
+    void addPicture(int id, String location, String refNumber);
+    void deletePicture(int id);
+    int getMaxID();
+    ArrayList<String> getAllPicturesForProject(String refNumber);
 }
