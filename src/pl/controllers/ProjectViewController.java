9103e4da61ce447fc6b4658874e300df0abf6cbc
Bálint <47783658+Roharx@users.noreply.github.com>
2023-05-01 11:02:02 +0200

    Added empty functions to the buttons

--- src/pl/controllers/ProjectViewController.java
+++ src/pl/controllers/ProjectViewController.java
@@ -1,4 +1,34 @@
 package pl.controllers;
 
-public class ProjectViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProjectViewController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    public void goBackPressed(ActionEvent actionEvent) {
+    }
+
+    public void saveBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void deleteBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void addBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void removeBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void drawBtn(ActionEvent actionEvent) {
+    }
+
+
 }
---
+++ src/pl/controllers/TechnicianViewController.java
@@ -0,0 +1,64 @@
+package pl.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TechnicianViewController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void createBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void updateBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void previewBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void saveBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void sendBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void deleteBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void searchFieldKeyPressed(KeyEvent keyEvent) {
+    }
+
+    public void createProjectPressed(ActionEvent actionEvent) {
+    }
+
+    public void allProjectsBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void publicProjectsBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void privateProjectsBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void logoutBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void minBtnPressed(ActionEvent actionEvent) {
+    }
+
+    public void maxBtnBtn(ActionEvent actionEvent) {
+    }
+
+    public void closeBtnPressed(ActionEvent actionEvent) {
+    }
+
+
+}
